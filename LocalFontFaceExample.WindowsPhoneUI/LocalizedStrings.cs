995b3364ec93b15ac2003b07bf05dbc6a8396d9e
Gabriel Monteagudo <gabrielmaldi@hotmail.com>
2014-03-03 18:43:14 -0200

    Created the solution using Visual Studio's "Windows Phone HTML5 App" template without any changes.

---
+++ LocalFontFaceExample.WindowsPhoneUI/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using LocalFontFaceExample.WindowsPhoneUI.Resources;
+
+namespace LocalFontFaceExample.WindowsPhoneUI
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
